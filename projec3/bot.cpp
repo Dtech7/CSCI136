59cc31f4ad24897b4c63867ee4d673082b51d26d
Neil D Felix <neild.felix@gmail.com>
2018-05-01 19:03:58 -0400

    Add files via upload

---
+++ projec3/bot.cpp
@@ -0,0 +1,134 @@
+/*
+Author: Neil Felix
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Project 3: Space Debris Collection
+
+Bots check cardinal and intercardinal directions for debris.
+If no debris is found at any of the direction,
+the bot will move in a random direction.
+//h
+//h
+//h
+//h
+//
+*/
+#include <cstdlib>
+#include <iostream>
+#include "bot.h"
+#include <stdlib.h>
+
+using namespace std;
+
+const int MAX_ROBOT_NUM = 50;
+
+int NUM;          // to remember number or robots
+int ROWS, COLS;   // map dimensions
+
+/* Initialization procedure, called when the game starts: */
+void onStart(int num, int rows, int cols, double mpr,
+             Area &area, ostream &log)
+{
+	NUM = num;   // save the number of robots and the map dimensions
+	ROWS = rows;
+	COLS = cols;
+
+	log << "Start!" << endl;
+}
+
+/* Deciding robot's next move */
+Action onRobotAction(int id, Loc loc, Area &area, ostream &log)
+{
+  int scan(int id, Loc loc, Area &area);
+	int row = loc.r; // current row and column
+	int col = loc.c;
+
+	if (area.inspect(row, col) == DEBRIS)
+		return COLLECT;
+	else
+	{
+	
+		
+		switch(scan(id, loc, area))
+		{
+		  case 0:
+			  return LEFT;
+		  case 1:
+			  return RIGHT;
+		  case 2:
+			  return UP;
+		  default:
+			  return DOWN;
+		}
+	} 
+}
+
+void onRobotMalfunction(int id, Loc loc, Area &area, ostream &log)
+{
+	log << "Robot " << id << " is damaged." << endl;
+}
+
+void onClockTick(int time, ostream &log)
+{
+	if (time % 100 == 0) log << time << " ";
+}
+
+//scans the all directions directly around the robot
+int scan(int id, Loc loc, Area &area)
+{
+  Loc nearestLoc(int id, Loc *locP, Loc loc, int arI);
+  
+  int row = loc.r;
+	int col = loc.c;
+	int nextMove;
+	int arI = 0;
+	Loc tempLoc[25]
+	Loc *locAr;
+	Loc nrstLoc;
+  
+  
+  for(int nrow = (row-2); nrow <= (row+2); nrow++)
+  {
+    for(int ncol = (col-2); ncol <= (col+2); ncol++)
+    {
+      if(area.inspect(nrow, ncol) == DEBRIS)
+      {
+        tempLoc[arI].r = nrow;
+        tempLoc[arI].c = ncol;
+        arI += 1;
+      } 
+    }
+  }
+  
+  locAr2 = new Loc[arI];
+  for(int i = 0; i < arI; i++)
+  {
+    locAr[i] = tempLoc[i];
+  }
+  
+  nrstLoc = nearestLoc(id, locAr, loc, arI);
+  
+  
+   
+}
+
+Loc nearestLoc(int id, Loc *locP, Loc loc, int arI)
+{
+  int curDist;
+  int nrstDist = 100;
+  Loc nrstLoc;
+  
+  for(int i=0; i < ari; i++)
+  {
+    curDist = abs((locP[i].r + locP[i].c) - (loc.r + loc.c));
+    if(curDist < nrstDist)
+    {
+      nrstLoc = locP[i];
+      nrstDist = curDist;
+    }
+  }
+  
+  return nrstLoc;
+}
+
+
